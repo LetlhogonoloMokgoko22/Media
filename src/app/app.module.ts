2df89fc9a5e1f63be9439a76beb9d00f9e01d0f1
LetlhogonoloMokgoko <47385111@mylife.unisa.ac.za>
2018-10-24 18:04:44 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions } from '@ionic-native/media-capture';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -22,6 +22,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    MediaCapture,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
